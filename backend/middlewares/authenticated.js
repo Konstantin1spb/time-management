4166db9f9809fa93812acf0985ac039686c312c1
Konstantin1spb <kevlen02@mail.ru>
2024-08-10 20:04:46 +0300

    fix jwt problem

--- backend/middlewares/authenticated.js
+++ backend/middlewares/authenticated.js
@@ -2,15 +2,20 @@ const { verify } = require("../helpers/token");
 const User = require("../models/User");
 
 module.exports = async function (req, res, next) {
-  const tokenData = verify(req.cookies.token);
+  try {
+    const tokenData = verify(req.cookies.token);
 
-  const user = await User.findOne({ _id: tokenData.id });
+    const user = await User.findOne({ _id: tokenData.id });
 
-  if (!user) {
-    res.send({ error: "Authenticated user not found" });
-    return;
+    if (!user) {
+      res.send({ error: "Authenticated user not found" });
+      return;
+    }
+
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log("ЗДЕСЬ", e);
+    next();
   }
-
-  req.user = user;
-  next();
 };
